155887ec55e443b39e5399cfa34cc2e4bdff05dc
zenath2 <35938856+zenath2@users.noreply.github.com>
2023-03-29 21:53:37 -0700

    Update 01_coprime.rb

--- 01_coprime.rb
+++ 01_coprime.rb
@@ -3,7 +3,14 @@
 # is 1. The method should return false otherwise. For example coprime?(25, 12)
 # is true because 1 is the only number that divides both 25 and 12.
 
-
+def coprime?(num_1, num_2)
+  (2..num_1) do |factor|
+    if num_2 % factor == 0
+      return false
+    end
+  end
+  return true
+end
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
 p coprime?(30, 9)     # => false
