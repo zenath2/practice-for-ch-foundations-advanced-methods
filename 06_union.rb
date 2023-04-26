ef296175b8ed6271a19105fa068937bf142e74e1
zenath2 <35938856+zenath2@users.noreply.github.com>
2023-04-26 02:15:32 -0700

    updated 4/26/23 -AZ

--- 06_union.rb
+++ 06_union.rb
@@ -1,6 +1,14 @@
 # Write a method, union, that accepts any number of arrays as arguments. The
 # method should return an array containing all elements of the given arrays.
+def union(*pita)
+  new_sam = []
+  pita.each do |falafels|
+    falafels.each do |fal|
+      new_sam << fal.flatten
+    end 
+  end
+  new_sam 
+end
 
-
-p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
-p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
+p union(["a", "b"], [1, 2, 5]) # => ["a", "b", 1, 2, 5]
+p union(["x", "y"], [true, false], [20, 21, 22]) # => ["x", "y", true, false, 20, 21, 22]
