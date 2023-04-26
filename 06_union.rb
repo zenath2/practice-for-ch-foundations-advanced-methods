686d5f9c94b8bb70df63e733b790a02898a29fcf
zenath2 <35938856+zenath2@users.noreply.github.com>
2023-04-26 02:10:27 -0700

    updated on 4/26/23
    refactored with ruby built-in function (flatten) and each method.

--- 06_union.rb
+++ 06_union.rb
@@ -1,6 +1,24 @@
 # Write a method, union, that accepts any number of arrays as arguments. The
 # method should return an array containing all elements of the given arrays.
 
+def union(*arrs) #method union taking in an unspecified number of arrays
+  flattened = []
+  #broken down, easy to understand method
+  arrs.each do |arr|
+    arr.each do |ele|
+      flattened << ele
+    end
+  end
+  flattened
+end
 
+# refactored method
+def union(*arrs)
+  flattened = []
+  arrs.each do |arr|
+    flattened << arrs.flatten
+  end
+  flattened
+end
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
